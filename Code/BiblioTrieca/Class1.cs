704c0792148520e9956f5a2d29e67a7cacba0225
Pučejdl Marek <marekpucejdl@gmail.com>
2025-08-09 12:24:10 +0000

    Upload New File

---
+++ Code/BiblioTrieca/Class1.cs
@@ -0,0 +1,43 @@
+﻿namespace BiblioTrieca
+{
+    interface TrieDatabase
+    {
+        void AddElement(string key, byte[] data);
+        byte[] ReadElement(string key);
+        void OverrideElement(string key, byte[] data);
+        void RemoveElement(string key);
+        byte[] ReadMetadata(string key);
+
+    }
+
+    public class TrieInFile
+    {
+        const int recordLength = 256;
+        //(26 chars + 10 numericals) * 4B + 111B data = 256 B
+
+        byte[] emptyRecord = new byte[256];
+
+        string adress;
+        uint nmbRecordsInDB; //In reality is one less than the true number of records in DB, shows last active record index
+
+
+        public TrieInFile(string adress)
+        {
+            this.adress = adress;
+            this.nmbRecordsInDB = 0;
+            FileStream fileStream = new FileStream(adress, FileMode.Create, FileAccess.Write);
+            BinaryWriter writer = new BinaryWriter(fileStream);
+            writer.Write(emptyRecord);
+            writer.Close();
+            fileStream.Close();
+        }
+
+        public void AddElement(string key, byte[] data)
+        {
+            uint activeIndex = 0;
+            FileStream fileStream = new FileStream(adress,FileMode.Open, FileAccess.ReadWrite);
+
+
+        }
+    }
+}
