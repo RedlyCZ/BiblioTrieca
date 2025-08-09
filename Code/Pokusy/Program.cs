3b8cf8f874a920d84b6c547da9264126aab3747d
Pučejdl Marek <marekpucejdl@gmail.com>
2025-08-09 12:24:51 +0000

    Upload New File

---
+++ Code/Pokusy/Program.cs
@@ -0,0 +1,14 @@
+﻿using BiblioTrieca;
+
+namespace Pokusy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            TrieInFile db = new TrieInFile("zkousime");
+            Console.WriteLine("mozna se i povedlo");
+        }
+    }
+}
